bb0e210474a39627c78424f8edfe5c1734d885d0
P1763 <p1763@student.jamk.fi>
2020-07-31 14:05:57 +0300

    Delete testing data

--- src/index.js
+++ src/index.js
@@ -5,49 +5,9 @@ import { Provider } from 'react-redux'
 // main app
 import App from '../containers/app';
 import newStore from './redux/expensify'
-import { addExpense} from './redux/expenses'
 import './firebase/firebase'
 
 const store = newStore()
-
-const e1 = store.dispatch(addExpense({
-    description: 'Rent',
-    note: 'House rent',
-    amount: 550,
-    createdAt: 4
-}))
-
-const e2 = store.dispatch(addExpense({
-    description: 'Monthly Rent',
-    note: 'Car rent',
-    amount: 50,
-    createdAt: 5
-}))
-
-const e3 = store.dispatch(addExpense({
-    description: 'Bill',
-    note: 'Electric bill',
-    amount: 60,
-    createdAt: 2
-}))
-
-
-const e5 = store.dispatch(addExpense({
-    description: 'Phone bill',
-    note: 'Phone bill',
-    amount: 20,
-    createdAt: 0
-}))
-
-const e4 = store.dispatch(addExpense({
-    description: 'Water bill',
-    note: 'Water bill',
-    amount: 10,
-    createdAt: 1
-}))
-
-
-const state = store.getState()
 
 const jsx = (
     <Provider store={store}>
